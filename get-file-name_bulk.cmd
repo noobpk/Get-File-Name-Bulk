a0876a60f635dfc5eecbbc10b7ab0c446c381979
noobpk <leethanhphuc@gmail.com>
2019-08-03 20:52:34 +0700

    update new feature

--- get-file-name_bulk.cmd
+++ get-file-name_bulk.cmd
@@ -9,20 +9,20 @@ pushd "%~dp0"
 cd /d "%~dp0"
 :MAINMENU
 echo. [94m[+]__author__: @noobpk[0m
-echo. [94m[+]__version__: 1.0[0m
+echo. [94m[+]__version__: 1.1[0m
 echo. [94m[+]__download + update__: https://github.com/noobpk/Get-File-Name-Bulk[0m
 echo. :=======================================================:
 echo.
 Echo.     [92m[1] Get filename - Support image extension ( *.jpg, *.png, *.bmp, *.gif, *jpge )[0m
 Echo.
-Echo.     [93m[^^!2] Update soon ^^!^^![0m
+Echo.     [92m[2] Get all filename in folder[0m
 echo. 
 Echo.     [93m[^^!3] Update soon ^^!^^![0m
 Echo.
 echo. :=======================================================:
 choice /C:123 /N /M "[+] Enter Your Choice [1,2,3] : "
 if errorlevel 3 goto :updatesoon
-if errorlevel 2 goto :updatesoon
+if errorlevel 2 goto :checkallfile
 if errorlevel 1 goto :checkfile
 ::===========================================================================
 :updatesoon
@@ -33,13 +33,14 @@ CLS
 GOTO MAINMENU
 ::===========================================================================
 :checkfile
+if exist "filename.txt" del "filename.txt"
 if not exist *.jpg if not exist *.png if not exist *.bpm if not exist *.gif if not exist *.jpge (goto :filenotfound) else (goto :filefound)	
-if exist "output.txt" del "output.txt"
 ::===========================================================================
 :filefound
-rem dir /b *.jpg *.png *.bmp *.gif *jpge >output.txt
-powershell -Com (ls *.jpg,*.png,*.bmp,*.gif,*jpge -File).BaseName > output.txt 
-echo. [92m[^>]Success - Check in file output.txt ^^!^^![0m
+rem dir /b *.jpg *.png *.bmp *.gif *jpge >filename.txt
+powershell -Com (ls *.jpg,*.png,*.bmp,*.gif,*jpge -File).BaseName > filename.txt 
+echo. [92m[^>]Success - Check in file filename.txt ^^!^^![0m
+powershell  explorer.exe $(pwd)\filename.txt
 echo. [96m[^>]Press any key to exit...[0m
 pause > nul
 echo. [Thanks for using ^^^^]
@@ -50,6 +51,31 @@ exit
 :filenotfound
 echo. [91m[x]Fail - Not found extension support in folder ^^!^^![0m
 echo. [92m[^>]Extension support: *.jpg, *.png, *.bmp, *.gif, *jpge[0m
+echo. [96m[^>]Press any key to exit...[0m
+pause > nul
+echo. [Thanks for using ^^^^]
+TIMEOUT /T 1 /nobreak > nul
+CLS
+exit
+::===========================================================================
+:checkallfile
+if exist "allfilename.txt" del "allfilename.txt"
+if not exist *.* (goto :allfilenotfound) else (goto :allfilefound)
+::===========================================================================
+:allfilefound
+powershell -Com (ls *.* -File).BaseName > allfilename.txt
+echo. [92m[^>]Success - Check in file allfilename.txt ^^!^^![0m
+powershell  explorer.exe $(pwd)\allfilename.txt
+echo. [96m[^>]Press any key to exit...[0m
+pause > nul
+echo. [Thanks for using ^^^^]
+TIMEOUT /T 1 /nobreak > nul
+CLS
+exit
+::===========================================================================
+:allfilenotfound
+echo. [91m[x]Fail - Not found any extension in folder ^^!^^![0m
+echo. [92m[^>]Check folder again[0m
 echo. [96m[^>]Press any key to exit...[0m
 pause > nul
 echo. [Thanks for using ^^^^]
